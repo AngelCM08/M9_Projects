ca5b35db34bdc2c86448b6f70e5dc0bcb79cb1d2
Ángel <angelcastromerino@gmail.com>
2023-01-25 17:50:30 +0100

    commit

---
+++ src/CursaRelleus/Testimoni.java
@@ -0,0 +1,40 @@
+package CursaRelleus;
+
+import java.util.Random;
+
+public class Testimoni {
+    private boolean agafat;
+
+    public Testimoni() {
+        this.agafat = false;
+    }
+
+    public synchronized void agafar(Atleta atleta) {
+        Random rd = new Random();
+        while(agafat) {
+            try {
+                wait();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        agafat = true;
+        notifyAll();
+
+        System.out.println(atleta.getName()+" ha agafat el testimoni, comença a correr...");
+
+        long time;
+        try {
+            Thread.sleep(time = rd.nextLong(3000)+2000);
+            atleta.setTemps(time);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public synchronized void deixar(Atleta atleta) {
+        System.out.println(atleta.getName()+" ha deixat el testimoni, ha fet un temps de "+atleta.getTemps());
+        agafat = false;
+        notifyAll();
+    }
+}
