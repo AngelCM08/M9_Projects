868d5991173a4c7b284bb3b7a897b1d9324c195c
Ángel <angelcastromerino@gmail.com>
2023-02-09 17:48:38 +0100

    commit

--- src/uf3/urls/Exercicis.java
+++ src/uf3/urls/Exercicis.java
@@ -3,13 +3,15 @@ package uf3.urls;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-public class ExerciciA {
-    private static void printContent(URL url, String label){
+public class Exercicis {
+    public static void exerciciA(URL url, String label){
         InputStream in;
         char[] cbuf = new char[512];
         int caractersLlegits;
@@ -29,11 +31,21 @@ public class ExerciciA {
         }
     }
 
-    public static void main(String[] args) {
+    public static void exerciciB(String input1, String input2){
         try {
-            ExerciciA.printContent(new URL(args[0]), args[1]);
-        } catch (MalformedURLException e) {
+            URL url = new URL("https://docs.google.com/forms/d/e/1FAIpQLSdV5QvhChK0fBpAMo5pN7sIvktqwHGu1vdoWJFvBguCeMvYUw/formResponse");
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setDoOutput(true);
+            OutputStreamWriter out = new OutputStreamWriter(con.getOutputStream());
+            out.write("entry.835030737=" + input1 + "&entry.1616686619=" + input2);
+            out.close();
+            InputStream in = con.getInputStream();
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
+    }
+    public static void main(String[] args) throws MalformedURLException {
+        //Exercicis.exerciciA(new URL(args[0]), args[1]);
+        Exercicis.exerciciB("Ángel Castro", "Si");
     }
 }
